e86d2519e17b8485b7caf4d839682deda60ca2c6
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-02-01 15:37:49 +0700

    first init

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link rel="stylesheet" href="home.css">
+</head>
+
+<body>
+  <div class="container">
+    <div class="auth-container">
+      <div class="title">
+        <span id="title-highlight">WeatherWise</span>
+        <span>Embrace Forecast Freedom</span>
+      </div>
+
+      <div class="option-container">
+        <a id="button" href="./login/">Signin</a>
+        <span id="button">OR</span>
+        <a id="button" href="./signup/">Signup</a>
+      </div>
+    </div>
+  </div>
+</body>
+
+</html>
